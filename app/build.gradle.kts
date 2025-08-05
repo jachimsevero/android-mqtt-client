651bd5d5c5395237c7c98fbb85043f02a5aaf230
Jachim Severo <jachimsevero@gmail.com>
2025-08-05 13:10:49 +0800

    Migrate to ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.dagger.hilt.android)
-  alias(libs.plugins.kotlin.kapt)
   alias(libs.plugins.kotlin.plugin.serialization)
+  alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -52,7 +52,7 @@ dependencies {
 
   implementation(libs.androidx.hilt.navigation.compose)
   implementation(libs.hilt.android)
-  kapt(libs.hilt.android.compiler)
+  ksp(libs.hilt.android.compiler)
   implementation(libs.androidx.navigation.compose)
   implementation(libs.kotlinx.serialization.json)
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.dagger.hilt.android) apply false
-  alias(libs.plugins.kotlin.kapt) apply false
   alias(libs.plugins.kotlin.plugin.serialization) apply false
+  alias(libs.plugins.devtools.ksp) apply false
 }
